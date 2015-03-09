450b315d8f0a8fe704c506292b96de63e1c1dcbb
Chris Ryan <creditdesign@gmail.com>
2015-03-09 16:06:59 +0000

    Replace canvas with image

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,23 @@
 (function() {
 	var d3url = "http://www.nature.com/polopoly_static/js/d3.v3.min.js";
 
-	/*	supports_canvas() - Check to see if the browser supports canvas
-		return true if canvas supported */
-	function supports_canvas() {
-  		return !!document.createElement('canvas').getContext;
-	}
-
 	var init = function($) {
 		$.when( $.getScript(d3url) ).then( function() {
 			var allImages = [];
 
+			/* Store with window Width to stop iOS resizing on scroll */
+			var windowWidth = $(window).width();
 			/* Remove 2px from the width to acount for the border */
 			var width = $("#content").width() - 2;
-			var height = Math.floor(width * 0.48);
 
 			var range;
 			var select;
 			var rangeWorking = false;
 
 			var outerWrapper = d3.select(".outerwrapper");
-			var canvas = outerWrapper.select(".widget-canvas")
-										.append("canvas")
-										.attr("width", width)
-										.attr("height", height);
-			var ctx = canvas.node().getContext("2d");
+
+			var imageCanvas = outerWrapper.select(".widget-canvas")
+										.append("img");
 
 			var dateDisplay = outerWrapper.select("#date");
 			
@@ -48,7 +41,8 @@
 				redraw the canvas and update the #date text */
 			function drawFrame (num) {
 				dateDisplay.text(allImages[num].alt);
-				ctx.drawImage(allImages[num], 0, 0, width, height);
+				imageCanvas.attr("src", allImages[num].src)
+							.attr("alt", allImages[num].alt);
 			}
 
 			/*	makeRange()
@@ -100,27 +94,16 @@
 			/*	resize()
 				Redraw and resize the canvas */
 			function resize () {
-				if($(window).width() != width){
-					var currentFrame;
+				if($(window).width() != windowWidth){
+					
+					windowWidth = $(window).width();
 					width = $("#content").width();
-					height = Math.floor(width * 0.48);
 
-					canvas.attr("width", width)
-							.attr("height", height);
-
-					if (range) {
-						currentFrame = range.node().value;
-					} else if (select) {
-						currentFrame = select.node().value;
-					} else {
-						currentFrame = 0;
-					}
-
-					drawFrame(currentFrame);
+					outerWrapper.style("width", width + "px");
 				}
 			}
 
-			/* Fill the canvas with frame 0 as soon as it is ready */
+			/* Fill the canvas image with frame 0 as soon as it is ready */
 			allImages[0].onload = function() {
 				drawFrame(0);
 			};
@@ -137,24 +120,19 @@
 
 		}, function () {
 			/*	D3 has failed to load so show the images */
-			$(".widget-images").css("display","block");
+			jQuery(".widget-images").css("display","block");
 		});
 	};
 
 	/*	Before calling init()
 		- check jQuery is loaded
-		- check the browser supports canvas
 		- check the images are hidden */
 	setTimeout(function() {
 		if (typeof jQuery !== 'undefined') {
-			if ( supports_canvas() ) {
-				if (jQuery(".widget-images").css("display") !== "none") {
-					jQuery(".widget-images").css("display","none");
-				}
-				init(jQuery);
-			} else {
-				jQuery(".widget-images").css("display","block");
+			if (jQuery(".widget-images").css("display") !== "none") {
+				jQuery(".widget-images").css("display","none");
 			}
+			init(jQuery);
 		} else {
 			setTimeout(arguments.callee, 60);
 		}
