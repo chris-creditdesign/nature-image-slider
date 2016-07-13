8c53af66c54bc773f8ba74fee96c3bad5d84125f
Chris Ryan <creditdesign@gmail.com>
2016-07-13 17:31:04 +0100

    First implementation of canvas slider

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,127 +1,140 @@
 (function() {
-	var d3url = "http://www.nature.com/polopoly_static/js/d3.v3.min.js";
-
 	var init = function($) {
-		$.when( $.getScript(d3url) ).then( function() {
-			var allImages = [];
+		var allImages = [];
 
-			/* Store with window Width to stop iOS resizing on scroll */
-			var windowWidth = $(window).width();
-			/* Remove 2px from the width to acount for the border */
-			var width = $("#content").width() - 2;
+		/* Store with window Width to stop iOS resizing on scroll */
+		var windowWidth = $(window).width();
+		
+		var width = $("#content").width() * 0.9;
+		var height = Math.floor(width * 0.62);
 
-			var range;
-			var select;
-			var rangeWorking = false;
+		var range;
+		var select;
+		var startingValue = 0.5;
 
-			var outerWrapper = d3.select(".outerwrapper");
+		var outerWrapper = $(".outerwrapper");
+		outerWrapper.find(".widget-selector").css("top", height * -1);
 
-			var imageCanvas = outerWrapper.select(".widget-canvas")
-										.append("img");
+		var imageCanvas = $("<canvas></canvas>");
+		var rangeInput = $("<input />");
 
-			var dateDisplay = outerWrapper.select("#date");
-			
-			var testInput = document.createElement("input");
-			testInput.setAttribute("type", "range");
-
-			var widgetImages = outerWrapper.select(".widget-images");
-			
-			/*	Select each of the images and push a new img object into
-				the allImages array with the relevant src and alt properties */
-			var images = widgetImages.selectAll("img")
-							.each(function(d,i) {
-								var thisImage = new Image();
-								thisImage.src = this.src;
-								thisImage.alt = this.alt;
-
-								allImages.push(thisImage);
-							});
-
-			/*	drawFrame() num = value of input or select
-				redraw the canvas and update the #date text */
-			function drawFrame (num) {
-				dateDisplay.text(allImages[num].alt);
-				imageCanvas.attr("src", allImages[num].src)
-							.attr("alt", allImages[num].alt);
-			}
-
-			/*	makeRange()
-				Append an input[type="range"] and call drawFrame on change */
-			function makeRange () {
-				range = outerWrapper.select(".widget-selector")
-											.append("input")
-											.attr("type","range")
-											.attr("min", 0)
-											.attr("max", (images[0].length - 1))
-											.attr("step", 1)
-											.attr("value", 0)
-											.on("input", function() {
-												rangeWorking = true;
-												drawFrame(this.value);
-											});				
-
-				/*	selection.on("input") doesn't seem to work in ie
-					repeating the call to drawFrame() here on slide end */
-				jQuery(".outerwrapper input[type='range']").click(function(){
-					this.blur();
-					this.focus();
-					drawFrame(this.value);
-				});
-			}
-
-			/*	makeSelect()
-				Append a select element and call drawFrame on change */
-			function makeSelect () {
-				select = outerWrapper.select(".widget-selector")
-								.append("select");
-
-				select.selectAll("option")
-					.data(images[0])
-					.enter()
-					.append("option")
-					.attr("value", function(d,i) {
-						return i;
-					})
-					.text(function(d,i) {
-						return d.alt;
-					});
-
-				select.on("change", function() {
-					drawFrame(this.value);
-				});
-			}
-
-			/*	resize()
-				Redraw and resize the canvas */
-			function resize () {
-				if($(window).width() != windowWidth){
-					
-					windowWidth = $(window).width();
-					width = $("#content").width();
-
-					outerWrapper.style("width", width + "px");
-				}
-			}
-
-			/* Fill the canvas image with frame 0 as soon as it is ready */
-			allImages[0].onload = function() {
-				drawFrame(0);
-			};
-
-			/*	call makeRange() if input[type="range"] is suported
-				call makeSelect() otherwise */
-			if (testInput.type !== "text") {
-				makeRange();
-			} else {
-				makeSelect();
-			}
-
-			window.onresize = resize;
-
-		}, function () {
-			/*	D3 has failed to load so show the images */
-			jQuery(".widget-images").css("display","block");
+		imageCanvas.attr({
+			"width": width,
+			"height": height
 		});
+
+		outerWrapper.find(".widget-canvas")
+			.append(imageCanvas);
+
+		var ctx = imageCanvas[0].getContext("2d");
+		ctx.strokeStyle = "#ffffff";
+		ctx.lineWidth = 2;
+		
+		var testInput = document.createElement("input");
+		testInput.setAttribute("type", "range");
+
+		var widgetImages = outerWrapper.select(".widget-images");
+		
+		/*	Select each of the images and push a new img object into
+			the allImages array with the relevant src */
+		var images = widgetImages.find("img");
+
+		$.each(images, function (key, value) {
+			var thisImage = new Image();
+			thisImage.src = this.src;
+			allImages.push(thisImage);
+		});
+
+		/*	drawFrame() num = value of input
+			redraw the canvas */
+		function drawFrame (num) {
+			ctx.clearRect(0,0,width,height);
+
+			ctx.fillStyle = "rgb(200,0,0)";
+	        ctx.fillRect (0, 0, width, height);
+			
+			/* Check that num is greater than 0 so we're not multiplying width by 0 */
+			if (num > 0) {
+				ctx.drawImage(allImages[0], 0, 0, (width * num), height, 0, 0, (width * num), height);
+				ctx.drawImage(allImages[1], (width * num), 0, (width * (1 - num)), height, (width * num), 0, (width * (1 - num)), height);
+			} else {
+				ctx.drawImage(allImages[1], 0, 0, width, height, 0, 0, width, height);
+			}
+			
+			ctx.beginPath();
+			ctx.moveTo((width * num),0);
+			ctx.lineTo((width * num),height);
+			ctx.closePath();
+			ctx.stroke();
+		}
+
+		/*	makeRange()
+			Append an input[type="range"] and call drawFrame on change */
+		function makeRange () {
+			
+			rangeInput.attr({
+				"type" : "range",
+				"min" : 0,
+				"max" : images.length - 1,
+				"step" : "any",
+				"value" : startingValue
+			}).css("height",height);
+
+			range = outerWrapper.find(".widget-selector");
+			range.append(rangeInput);
+			
+			rangeInput.bind("input", function() {
+				drawFrame(parseFloat(this.value));
+			});
+		}
+
+		/*	makeSelect()
+			Append a select element and call drawFrame on change */
+		function makeSelect () {
+			console.log("Let's call the whole thing off");
+		}
+
+		/*	resize()
+			Redraw and resize the canvas */
+		function resize () {
+			if($(window).width() != windowWidth){
+				
+				windowWidth = $(window).width();
+				width = $("#content").width() * 0.9;
+				height = Math.floor(width * 0.62);
+
+				imageCanvas.attr({
+					"width": width,
+					"height": height
+				});
+
+				ctx.strokeStyle = "#ffffff";
+				ctx.lineWidth = 2;
+
+				rangeInput.attr({"value": startingValue});
+				drawFrame(startingValue);
+			}
+		}
+
+		/* Fill the canvas image with frame 0 as soon as it is ready */
+		allImages[0].onload = function() {
+			drawFrame(startingValue);
+		};
+
+		allImages[1].onload = function() {
+			drawFrame(startingValue);
+		};
+
+		/*	call makeRange() if input[type="range"] is suported
+			call makeSelect() otherwise */
+		if (testInput.type !== "text") {
+			makeRange();
+		} else {
+			makeSelect();
+		}
+
+		window.onresize = resize;
+
 	};
 
 	/*	Before calling init()
